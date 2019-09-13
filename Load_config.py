17b3831ab898fa0cc63552f8a56c450c5e631711
hindu <bhinduja08@gmail.com>
2019-09-13 13:27:13 +0530

    include load config methods

--- Load_config.py
+++ Load_config.py
@@ -2,3 +2,11 @@ import yaml
 
 def load_config(config_file):
     with open(config_file, 'r') as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+config = load_config('config.yaml')
+
+print(config)
